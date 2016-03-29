455836296c960e1684ac7da9bf92acffc38a2814
naorlivne <naorlivne@gmail.com>
2016-03-29 12:48:36 +0300

    writing to dev null so the logs wont explode

--- etcd_set_ttl.sh
+++ etcd_set_ttl.sh
@@ -3,7 +3,7 @@
 while :
 do
 #set etcd key
-curl http://$ETCD_SERVER:$ETCD_PORT/v2/keys$ETCD_SETTER_KEY -XPUT -d value=$ETCD_SETTER_VALUE -d ttl=$ETCD_SETTER_TTL
+curl http://$ETCD_SERVER:$ETCD_PORT/v2/keys$ETCD_SETTER_KEY -XPUT -d value=$ETCD_SETTER_VALUE -d ttl=$ETCD_SETTER_TTL > /dev/null
 #wait half the ttl
 sleep $(($ETCD_SETTER_TTL/2))
 done
